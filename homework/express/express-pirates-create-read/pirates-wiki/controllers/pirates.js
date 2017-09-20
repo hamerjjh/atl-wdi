efd482c2989ec525a6df7b04e8fc82a039ac294e
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-09-20 19:03:04 -0400

    pirate hw

--- homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,42 @@
 //your code below
+//requirements
+var express = require("express");
+var router = express.Router();
+var data = require('../models/data.js');
+
+//* INDEX TOOLS */
+router.get('/', (req, res) => {
+    res.render('pirates/index', {
+        pirates: data
+    });
+});
+
+/* NEW PIRATES */
+router.get('/new', (req,res) => {
+    res.render('pirates/new');
+})
+
+/*SHOW PIRATES */
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pirates = data[id];
+      if (!pirates){
+          res.render('pirates/show', {
+              error: "No pirates found with this ID"
+          })
+      } else {
+    res.render('pirates/show', {
+        pirates
+    })
+  }
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const newPirates = req.body;
+    data.push(newPirates);
+    res.redirect('/pirates');
+});
+
+//exports
+module.exports = router;
--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,31 @@
 //your code here
+/* packages */
+var express     = require('express');
+var hbs         = require('hbs');
+/* app settings*/
+var app         = express();
+var port        = process.env.PORT || 3002;
+const bodyParser = require('body-parser');
+const pirateController = require('./controllers/pirates.js');
+
+/*log*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/*Views*/
+app.set('view engine', 'hbs');
+
+//controllers for `/pirates` resource
+app.use("/pirates", pirateController);
+
+/* HOME */
+app.get('/', function(req,res) {
+    res.send('This is our Home Page');
+  });
+
+
+
+// Start server
+app.listen(port, function() {
+    console.info('Server Up -- Ready to serve hot todos on port', port,"//", new Date());
+  });
