754d14b9288575e258f39bfd0057fb28417ffc8c
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-09-20 00:39:43 -0400

    Completed GET routes

--- homework/express/pizza-express/server.js
+++ homework/express/pizza-express/server.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    response.send("Welcome to Pizza Express!");
+  })
+
+  app.get('/topping/:type', function(req, res, next) {
+    
+        res.send(`${req.params.type} pizza! Good Choice`);
+    });
+
+ app.get('/order/:amount/:size', function(req, res, next) {
+
+     res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+ });
+
+
+
 const port = 3000;
 
 app.listen(port, function(){
